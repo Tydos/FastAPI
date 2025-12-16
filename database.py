ba50263e7e21b8a242a2055389b18dde8fc0c209
Prasad Jawale <80669588+Tydos@users.noreply.github.com>
2025-12-16 09:50:25 -0600

    Added a few additonal routes

--- database.py
+++ database.py
@@ -2,24 +2,36 @@ from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 import os
 
+#Connect to MongoDB using URI from environment variables
+from dotenv import load_dotenv
+load_dotenv()
 URI = os.getenv("URI")
 client = MongoClient(URI, server_api=ServerApi('1'))
+database = client["sample_mflix"]
+movies_collection = database["movies"]
 
-def setup_database():
+def check_connection():
     try:
-        client.admin.command('ping')
-        print("Successfully connected to MongoDB")
+        client.admin.command('ismaster')
+        print("MongoDB connection: Successful")
     except Exception as e:
-        print("Connection failed:", e)
-        raise
+        print("MongoDB connection: Failed", e)
 
-    database = client["sample_mflix"]
-    return database["movies"]
 
-def fetch_movies(limit=1):
-    movies_collection = setup_database()
-    cursor = movies_collection.find({}, {"_id": 0, "title": 1}).limit(limit)
-    return [movie["title"] for movie in cursor if "title" in movie]
+def fetch_movies_list(limit=1):
+    search_query = {}
+    projection_query = {"_id": 0, "title": 1}
+    cursor = movies_collection.find(search_query, projection_query).limit(limit)
+    return [movie.get("title") for movie in cursor]
+
+def search_movie(movie_title):
+    search_query = {"title":movie_title}
+    projection_query = {"_id": 0, "title": 1,"imdb.rating": 1}
+    cursor = movies_collection.find(search_query,projection_query)
+    movie_info = {}
+    return [movie for movie in cursor]
 
 if __name__ == "__main__":
-    print(fetch_movies())
+    check_connection()
+    # movies = search_movie("The Italian")
+    # print(movies)
